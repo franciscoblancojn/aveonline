881f9184fba4ad8532aa483c408cd823a63d1226
Francisco Blanco <blancofrancisco34@gmail.com>
2020-08-13 03:21:25 -0400

    Version Final checkout

--- src/js/contraentrega.js
+++ src/js/contraentrega.js
@@ -1,9 +1,9 @@
-function init_WC_contraentrega() {
+function init_WC_contraentrega(load = true) {
     WC_contraentrega = document.getElementById('payment_method_WC_contraentrega')
     if (WC_contraentrega == null) return;
 
     WC_contraentrega.rebug = function () {
-        console.log(this.checked);
+        //console.log(this.checked);
         if (this.checked) {
             document.body.classList.add('wc_contraentrega_on')
         } else {
@@ -12,12 +12,16 @@ function init_WC_contraentrega() {
         active = document.documentElement.querySelector('.shipping_method[checked="checked"][id*="wc_contraentrega"]')
         if (active == null) return;
         if (this.checked) {
-            document.getElementById(active.id.split("wc_contraentrega_off").join("wc_contraentrega_on")).click()
+            e = document.getElementById(active.id.split("wc_contraentrega_off").join("wc_contraentrega_on"))
         } else {
-            document.getElementById(active.id.split("wc_contraentrega_on").join("wc_contraentrega_off")).click()
+            e = document.getElementById(active.id.split("wc_contraentrega_on").join("wc_contraentrega_off"))
+        }
+        if (!e.checked) {
+            e.click()
         }
     }
-    WC_contraentrega.rebug()
+    if (load)
+        WC_contraentrega.rebug()
 
     wc_payment_method = document.documentElement.querySelectorAll('.wc_payment_method')
     for (var i = 0; i < wc_payment_method.length; i++) {
@@ -25,15 +29,10 @@ function init_WC_contraentrega() {
             WC_contraentrega.rebug()
         }
     }
-    order_review = document.getElementById('order_review')
-    order_review.onchage = function () {
-        init_WC_contraentrega()
-    }
 }
-
 window.onload = function () {
     init_WC_contraentrega()
-    const panoptico = new MutationObserver((mutaciones, observer) => init_WC_contraentrega());
+    const panoptico = new MutationObserver((mutaciones, observer) => init_WC_contraentrega(false));
     panoptico.observe(document.getElementById('order_review'), { attributes: true, childList: true, subtree: true })
 
 }
