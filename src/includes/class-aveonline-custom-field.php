2490eec63977ffe6023c9ed45e4d23a35f044683
Francisco Blanco <blancofrancisco34@gmail.com>
2020-09-21 20:51:53 -0500

    update Status Guia 100%

--- src/includes/class-aveonline-custom-field.php
+++ src/includes/class-aveonline-custom-field.php
@@ -102,3 +102,30 @@ function woocommerce_custom_valor_declarado_save($post_id)
         update_post_meta($post_id, '_custom_valor_declarado', esc_attr($woocommerce_custom_valor_declarado));
 }
 add_action('woocommerce_process_product_meta', 'woocommerce_custom_valor_declarado_save');
+
+
+
+//state_guia
+//show
+add_action( 'woocommerce_admin_order_data_after_shipping_address', 'show_state_guia_order', 10, 1 );
+function show_state_guia_order( $order ) {    
+    $order_id = $order->get_id();
+    $e = get_post_meta( $order_id, 'state_guia', true );
+    if ( $e ) {
+        echo "State Guia <hr>";
+        for ($i=0; $i < count($e) ; $i++) { 
+            if(isset($e[$i]["status"]) && $e[$i]["status"] == "ok"){
+                $estado = $e[$i]["estado"];
+                for ($j=0; $j < count($estado); $j++) { 
+                    echo "<p>";
+                    echo $estado[$j]["nombre_estado"];
+                    echo "<br>";
+                    echo $estado[$j]["fecha"];
+                    echo "</p>";
+                }
+                echo "<hr>";
+            }
+        }
+        echo "<hr>";
+    }
+}
--- src/includes/class-aveonline-update-guia.php
+++ src/includes/class-aveonline-update-guia.php
@@ -1,6 +1,37 @@
 <?php
-
+require_once '../../../../../wp-blog-header.php';
 $data = json_decode(file_get_contents('php://input'), true);
 if(isset($data)){
     $_POST = $data;
+}
+if(isset($_POST["status"]) && $_POST["status"] == "ok"){
+    if(!isset($_POST["guia"])){
+        echo "Error, guia requerida";
+        exit;
+    }
+    if(!isset($_POST["pedido_id"])){
+        echo "Error, pedido_id requerida";
+        exit;
+    }
+    if(!isset($_POST["estado"])){
+        echo "Error, estado requerida";
+        exit;
+    }
+
+    $guia = $_POST["guia"];
+    $order_id = $_POST["pedido_id"];
+    $estado = $_POST["estado"];
+
+    $order = wc_get_order($order_id);
+
+    if($order == null){
+        echo "Error, invalid pedido_id";
+        exit;
+    }
+    $state_guia = get_post_meta( $order_id, 'state_guia', true );
+    if($state_guia == null){
+        $state_guia = array();
+    }
+    $state_guia[] = $_POST;
+    echo (update_post_meta( $order_id, 'state_guia', $state_guia ))?"ok":"Error, update fail";
 }
