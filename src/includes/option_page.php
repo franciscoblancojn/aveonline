5761f68a131facc6e7e29578ecabd8a08c6a28f3
franciscoblancojn <blancofrancisco34@gmail.com>
2021-05-13 12:58:20 -0500

    btn

--- src/includes/option_page.php
+++ src/includes/option_page.php
@@ -14,9 +14,41 @@ function register_Aveonline_settings() {
 
 function Aveonline_settings_page(){
     ?>
-    <h1>
+    <h1 class="title">
         Aveonline
     </h1>
+    <div class="content-a">
+        <a href="<?=get_admin_url()?>options-general.php?page=recogida_aveonline" class="btnA">
+            Recogida
+        </a>
+        <a href="<?=get_admin_url()?>options-general.php?page=relacion_envio_aveonline" class="btnA">
+            Relacion de Envio
+        </a>
+    </div>
+    <style>
+        .title{
+            font-size: 5rem;
+        }
+        .content-a{
+            display:flex;
+        }
+        .btnA{
+            padding: 20px 40px;
+            max-width: 100%;
+            border: 3px solid;
+            border-radius: 10px;
+            margin-right: 20px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+            font-size: 20px;
+            text-transform: uppercase;
+            text-decoration:none;
+            background:#fff;
+            color: #1d2327;
+        }
+    </style>
     <?php
 }
 
